9737e3a1c75729734ea14421198e3c1c4a5f17f1
Daniel Enriquez <camus_mm@hotmail.com>
2018-02-12 11:49:06 -0600

    -Fix

--- ListaLigada/ListaLigada/Nodo.h
+++ ListaLigada/ListaLigada/Nodo.h
@@ -18,3 +18,51 @@ private:
 	Nodo* prev;
 };
 
+template <class T>
+Nodo<T>::Nodo(T Value)
+{
+	this->Value = Value;
+	this->next = NULL;
+	this->prev = NULL;
+}
+
+template<class T>
+Nodo<T>::~Nodo()
+{
+}
+
+template<class T>
+T Nodo<T>::getValue()
+{
+	return this->Value;
+}
+
+template<class T>
+void Nodo<T>::setValue(T value)
+{
+	this->Value = Value;
+}
+
+template<class T>
+Nodo<T>* Nodo<T>::getNext()
+{
+	return this->next;
+}
+
+template<class T>
+void Nodo<T>::setNext(Nodo* next)
+{
+	this->next = next;
+}
+
+template<class T>
+Nodo<T>* Nodo<T>::getPrev()
+{
+	return this->prev;
+}
+
+template<class T>
+void Nodo<T>::setPrev(Nodo* prev)
+{
+	this->prev = prev;
+}
